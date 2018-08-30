d113fd24cc9987c9228ca61d8850af84436b771b
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2018-08-30 16:48:11 +0200

    Travis get no conf

--- src/photon-app.js
+++ src/photon-app.js
@@ -9,7 +9,7 @@ import '@photon-elements/photon-ide-elements/photon-query';
 class PhotonApp extends LitElement {
   _render({debug}) {
     return html`
-      <photon-query confPath='/photon-conf.yaml' debug?='${debug}'></photon-query>
+      <photon-query confPath='photon-conf.yaml' debug?='${debug}'></photon-query>
     `;
   }
 
